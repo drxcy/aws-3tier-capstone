f63ccf9214ef9151adfbf0fa43f05e97c67c81e4
drxcy <rachitbahuguna5@gmail.com>
2025-08-20 20:29:30 +0530

    some bug fixes in index.js

--- 3-Tier-Aws-App/app/backend/index.js
+++ 3-Tier-Aws-App/app/backend/index.js
@@ -1,14 +1,23 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
 const PORT = 5000;
 
+
+
+const app = express();
 app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Backend API is running 🚀");
+});
+
 
 app.get("/api/message", (req, res) => {
-  res.json({ message: "Hello from the Node.js Backend!" });
+  res.json({ message: "Hello from Node.js backend!" });
 });
 
 app.listen(PORT, () => {
-  console.log(`Backend running on http://localhost:${PORT}`);
-})
+  console.log(`✅ Server running on port ${PORT}`);
+});
