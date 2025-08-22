d305c0a878c72dbe86eddd12e6ad6260c6ac6c1f
drxcy <rachitbahuguna5@gmail.com>
2025-08-23 00:35:43 +0530

    updation on test file for configuration

--- 3-Tier-Aws-App/app/db/test.js
+++ 3-Tier-Aws-App/app/db/test.js
@@ -1,18 +1,18 @@
-import { Client } from 'pg'
+// import { Client } from 'pg'
 
-const client = new Client({
-  user: "postgres",
-  host: "localhost",  // or "db" if inside Docker compose
-  database: "appdb",
-  password: "postgres",
-  port: 5432,
-});
+// const client = new Client({
+//   user: "postgres",
+//   host: "localhost",  // or "db" if inside Docker compose
+//   database: "appdb",
+//   password: "postgres",
+//   port: 5432,
+// });
 
-client.connect()
-  .then(() => console.log("✅ Connected to Postgres"))
-  .then(() => client.query("SELECT NOW();"))
-  .then(res => {
-    console.log("DB Time:", res.rows[0]);
-  })
-  .catch(err => console.error("❌ Connection error", err))
-  .finally(() => client.end());
+// client.connect()
+//   .then(() => console.log("✅ Connected to Postgres"))
+//   .then(() => client.query("SELECT NOW();"))
+//   .then(res => {
+//     console.log("DB Time:", res.rows[0]);
+//   })
+//   .catch(err => console.error("❌ Connection error", err))
+//   .finally(() => client.end());
